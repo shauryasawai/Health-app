ef59ac2b70b3f3d9e96f471c9584de027a501b94
Shauryaman Sawai <sawaisushil@gmail.com>
2024-08-06 18:50:23 +0530

    starting PAges

--- base/form.py
+++ base/form.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import CallBooking
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.forms import UserCreationForm
 
 class CallBookingForm(forms.ModelForm):
     class Meta:
@@ -15,3 +18,49 @@ class PredictionForm(forms.Form):
 
 class MoodForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea, label='Enter your text')
+class SignUpForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = None
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    
+    name = forms.CharField(label='name', max_length=100)
+    
+    phone_no = forms.CharField(max_length=20)
+
+    email = forms.EmailField( label='Email', required=True)
+    
+    address = forms.CharField()
+
+
+    class Meta:
+        model = User
+        fields = ['username',     'email', 
+                  
+                  'password1', 'password2',  'name',  'phone_no', 'address' ]
+        
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email address is already in use.")
+        return email
+
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField()
--- base/urls.py
+++ base/urls.py
@@ -5,10 +5,17 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from .views import mood_predictor_view
 from .views import predictor_view
+from . import views
+from .views import logout_view,about_us
+from .views import custom_login
+from .views import reset_password
+from .views import CustomPasswordResetCompleteView
+
 
 
 urlpatterns=[
-  path('', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
+  path('', views.login_view , name='login'),
+  path('signup/', views.signup, name='signup'),
   path('accounts/profile/', views.home_view , name='Home'),
   path('fitness/', views.fitness_view , name='Fitness'),
   path('health/', views.health_view, name='Health'),
@@ -16,10 +23,15 @@ urlpatterns=[
   path('nutrition/', views.nutrition_view, name='nutrition'),
   path('book_call/', views.book_call, name='book_call'),
   path('success/', views.success, name='success'),
-  path('chat/', views.chat_view, name='chat'),
   path('predict/', views.predict_view, name='predict'),
   path('quizzes/', views.quiz_list, name='quiz_list'),
   path('quiz/<int:quiz_id>/', views.take_quiz, name='take_quiz'),
   path('mood/', mood_predictor_view, name='mood_predictor'),
-  path('predictors/', predictor_view, name='predictor')
+  path('predictors/', predictor_view, name='predictor'),
+  path('logout/', logout_view, name='logout'),
+  path('custom_login/', custom_login, name='custom_login'),
+  path('forgot-password/', views.forgot_password, name='forgot-password'),
+  path('reset-password/',reset_password, name='reset-password'),
+  path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+  path('logout/', logout_view, name='logout'),
 ]
